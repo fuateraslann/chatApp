c456e5cb8462378ad24ab409529b57b616c2f11b
fuat <fuat.eraslan@evend.com.tr>
2020-08-07 08:56:04 +0000

    mongoose

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-
+var mongoose=require('mongoose');
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
+mongoose.connect('mongodb+srv://fuat:123e-vend456@cluster0.f8y8n.mongodb.net/Messages?retryWrites=true&w=majority',{useMongoClient: true} )
+	.then(()=> {
+		console.log('mongodb bağlantısı sağlandı');
+	
+	})
+	.catch((err)=>{
+		console.log('mongodb bağlantı hatası.');
+	
+	});
 io.on('connection', (socket) => {
   socket.on('chat message', (msg)=> {
   	console.log('Message: '+msg);
